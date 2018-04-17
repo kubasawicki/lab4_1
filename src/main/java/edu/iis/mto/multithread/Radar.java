9ee434cb51a0b96a5714502e89738c30dc8bbdb4
radoslaw.adamus <radoslaw.adamus@p.lodz.pl>
2018-04-17 14:23:40 +0200

    initial commit

---
+++ src/main/java/edu/iis/mto/multithread/Radar.java
@@ -0,0 +1,29 @@
+package edu.iis.mto.multithread;
+
+public class Radar {
+
+    private PatriotBattery battery;
+
+    public Radar(PatriotBattery missle) {
+        this.battery = missle;
+    }
+
+    public void notice(Scud enemyMissle) {
+        launchPatriot();
+    }
+
+    private void launchPatriot() {
+        Runnable launchPatriotTask = new Runnable() {
+
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    battery.launchPatriot();
+                }
+            }
+        };
+
+        Thread launchingThread = new Thread(launchPatriotTask);
+        launchingThread.start();
+    }
+}
