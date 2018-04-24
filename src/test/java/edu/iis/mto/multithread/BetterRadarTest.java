06645b9a8c0737eca7d5e9e9e1f3aa001dacf721
Kuba Sawicki <202289@edu.p.lodz.pl>
2018-04-24 17:28:07 +0200

    Added rule to BetterRadarTest.java

--- src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
+++ src/test/java/edu/iis/mto/multithread/BetterRadarTest.java
@@ -3,10 +3,18 @@ package edu.iis.mto.multithread;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
+
+import org.junit.Rule;
 import org.junit.Test;
 
-public class BetterRadarTest {
+import edu.iis.mto.multithread.RepeatRule.Repeat;
 
+public class BetterRadarTest {
+	
+    @Rule
+    public RepeatRule repeatRule = new RepeatRule();
+
+    @Repeat(times = 1000)
     @Test
     public void launchPatriotOnceWhenNoticesAScudMissle() {
         PatriotBattery batteryMock = mock(PatriotBattery.class);
